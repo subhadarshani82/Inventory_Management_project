d05f933d7c6164a3f1c91f633d87ea39f08b0cad
subhadarshani <subhadarshani82@gmail.com>
2025-03-05 13:07:12 +0530

    Add all apps to settins.py file INSTALLED_APP.

--- inventoryManagement/settings.py
+++ inventoryManagement/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'products',
+    'inventory',
+    'orders',
+    'reports',
 ]
 
 MIDDLEWARE = [
